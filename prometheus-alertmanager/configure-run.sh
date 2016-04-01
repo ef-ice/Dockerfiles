41e870a1a01542491bbb351f44f7f5c7e4b8401b
vaishakhnadgir <vaishakhnadgir>
2016-04-01 18:06:32 +0100

    Fix sed issues

--- prometheus-alertmanager/configure-run.sh
+++ prometheus-alertmanager/configure-run.sh
@@ -13,7 +13,7 @@ if [ -z "$SLACK_CHANNEL" ]; then
    exit 1
 fi
 
-sed -i "s/SLACK_API_URL/$SLACK_API_URL/g" /etc/alertmanager/alertmanager.yml
-sed -i "s/SLACK_CHANNEL/$SLACK_CHANNEL/g" /etc/alertmanager/alertmanager.yml
+sed -i s,SLACK_API_URL,$SLACK_API_URL,g /etc/alertmanager/alertmanager.yml
+sed -i s,SLACK_CHANNEL,$SLACK_CHANNEL,g /etc/alertmanager/alertmanager.yml
 
 /bin/alertmanager "$@"
