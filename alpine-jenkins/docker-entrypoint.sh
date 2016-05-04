b97ae1f4aac57d703c2202c96f86a824577012f6
vnadgir-ef <vaishakh.nadgir@ef.com>
2016-05-04 12:49:44 +0100

    Reload all necesary plugins by default

--- alpine-jenkins/docker-entrypoint.sh
+++ alpine-jenkins/docker-entrypoint.sh
@@ -2,9 +2,13 @@
 
 set -eo pipefail
 
+
+ln -s  ${JENKINS_INSTALL_DIR}/plugins ${JENKINS_HOME}/plugins
+
+
 # If there are any arguments then we want to run those instead
 if [[ "$1" == "-"* || -z $1 ]]; then
-  exec java -jar /opt/jenkins.war "$@"
+  exec java -jar ${JENKINS_INSTALL_DIR}/jenkins.war "$@"
 else
   exec "$@"
 fi
