5f6fadfd52b893d97f0db93e7ea7ce9116e0f141
vnadgir-ef <vaishakh.nadgir@ef.com>
2016-05-05 10:54:41 +0100

    Fix symlink issue

--- alpine-jenkins/docker-entrypoint.sh
+++ alpine-jenkins/docker-entrypoint.sh
@@ -3,7 +3,7 @@
 set -eo pipefail
 
 
-ln -s  ${JENKINS_INSTALL_DIR}/plugins ${JENKINS_HOME}/plugins
+ln -s  ${JENKINS_INSTALL_DIR}/plugins ${JENKINS_HOME}
 
 
 # If there are any arguments then we want to run those instead
