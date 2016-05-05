3690f25534b47c38106db5c3fa4d178ea3ed7881
Kirill Kulikov <kirill.kulikov@gmail.com>
2016-05-05 12:26:00 +0100

    alpine jenkins dockerfile clean up

--- alpine-jenkins/docker-entrypoint.sh
+++ alpine-jenkins/docker-entrypoint.sh
@@ -2,10 +2,6 @@
 
 set -eo pipefail
 
-
-ln -s  ${JENKINS_INSTALL_DIR}/plugins ${JENKINS_HOME}
-
-
 # If there are any arguments then we want to run those instead
 if [[ "$1" == "-"* || -z $1 ]]; then
   exec java -jar ${JENKINS_INSTALL_DIR}/jenkins.war "$@"
