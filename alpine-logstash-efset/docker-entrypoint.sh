6dbd3162ec48f4e9d9811ba030fd8567d8e6d211
Kirill Kulikov <kirill.kulikov@gmail.com>
2016-06-15 16:34:50 +0100

    logstash updated to get rid of local file input

--- alpine-logstash-efset/docker-entrypoint.sh
+++ alpine-logstash-efset/docker-entrypoint.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
-
 set -eo pipefail
-
-if [ -z "$FILE_PATH" ]; then
-   echo "Missing FILE_PATH, e.g. /var/log/journald.log"
-   exit 1
-fi
 
 if [ -z "$ELASTICSEARCH_NODES" ]; then
    echo "Missing ELASTICSEARCH_NODES, e.g. [\"172.24.40.1:9200\",\"172.24.40.2:9200\"]"
    exit 1
 fi
 
-sed -i "s~FILE_PATH~$FILE_PATH~g" /opt/logstash/config/logstash.conf
 sed -i "s~ELASTICSEARCH_NODES~$ELASTICSEARCH_NODES~g" /opt/logstash/config/logstash.conf
 
 cat /opt/logstash/config/logstash.conf
