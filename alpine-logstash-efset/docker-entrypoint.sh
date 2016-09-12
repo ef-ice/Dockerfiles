b5718f6541299285cea59df6652ab06267799553
Kirill Kulikov <kirill.kulikov@gmail.com>
2016-09-12 14:19:21 +0100

    elk stack updated to 3.4.0

--- alpine-logstash-efset/docker-entrypoint.sh
+++ alpine-logstash-efset/docker-entrypoint.sh
@@ -6,13 +6,13 @@ if [ -z "$ELASTICSEARCH_NODES" ]; then
    exit 1
 fi
 
-sed -i "s~ELASTICSEARCH_NODES~$ELASTICSEARCH_NODES~g" /opt/logstash/config/logstash.conf
+sed -i "s~ELASTICSEARCH_NODES~$ELASTICSEARCH_NODES~g" /logstash/config/logstash.conf
 
-cat /opt/logstash/config/logstash.conf
+cat /logstash/config/logstash.conf
 
 # If there are any arguments then we want to run those instead
 if [[ "$1" == "-"* || -z $1 ]]; then
-  exec /opt/logstash/bin/logstash agent -f /opt/logstash/config/logstash.conf "$@"
+  exec /logstash/bin/logstash agent -f /logstash/config/logstash.conf "$@"
 else
   exec "$@"
 fi
