a116d8b33385366ac045b35c362c6d5bcba390b3
vnadgir-ef <vaishakh.nadgir@ef.com>
2016-10-10 22:51:01 +0100

    Use local resolver configured in /etc/resolv.conf

--- efset-nginx-data/docker-entrypoint.sh
+++ efset-nginx-data/docker-entrypoint.sh
@@ -6,6 +6,8 @@ if [ -z "$PROXY_PASS_URL" ]; then
    exit 1
 fi
 
-sed -i "s PROXY_PASS_URL $PROXY_PASS_URL g" /etc/nginx/nginx.conf
+export NAMESERVERS=$(cat /etc/resolv.conf | grep "nameserver" | awk '{print $2}' | tr '\n' ' ')
+sed -i "s|resolver NAMESERVERS;|resolver $NAMESERVERS;|g" /etc/nginx/nginx.conf
+sed -i "s|PROXY_PASS_URL|$PROXY_PASS_URL|g" /etc/nginx/nginx.conf
 
 nginx -g 'daemon off;'
