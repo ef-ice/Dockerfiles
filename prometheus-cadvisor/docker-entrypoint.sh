fb2646fd8db8d79cc5a9658f7c95b42cb1586490
vnadgir-ef <vaishakh.nadgir@ef.com>
2016-12-19 23:58:35 +0000

    Attempt to scrape cassandra target

--- prometheus-cadvisor/docker-entrypoint.sh
+++ prometheus-cadvisor/docker-entrypoint.sh
@@ -13,7 +13,13 @@ if [ -z "$ENVIRONMENT_TAG_REGEX" ]; then
    exit 1
 fi
 
+if [ -z "$CATEGORY_TAG_REGEX" ]; then
+   echo "Missing CATEGORY_TAG_REGEX environment variable"
+   exit 1
+fi
+
 sed -i s,PLATFORM_TAG_REGEX,$PLATFORM_TAG_REGEX,g /etc/prometheus/prometheus.yml
 sed -i s,ENVIRONMENT_TAG_REGEX,$ENVIRONMENT_TAG_REGEX,g /etc/prometheus/prometheus.yml
+sed -i s,CATEGORY_TAG_REGEX,$CATEGORY_TAG_REGEX,g /etc/prometheus/prometheus.yml
 
 /bin/prometheus "$@"
