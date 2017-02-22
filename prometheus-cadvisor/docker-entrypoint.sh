7f87b1d2523f662954d4e1b4d7e5d06e0e8909b1
Kirill Kulikov <kirill.kulikov@gmail.com>
2017-02-22 16:18:27 +0000

    aws_region config param added for prometheus-cadvisor

--- prometheus-cadvisor/docker-entrypoint.sh
+++ prometheus-cadvisor/docker-entrypoint.sh
@@ -18,8 +18,16 @@ if [ -z "$CATEGORY_TAG_REGEX" ]; then
    exit 1
 fi
 
+if [ -z "$AWS_REGION" ]; then
+   echo "Missing AWS_REGION environment variable"
+   exit 1
+fi
+
 sed -i s,PLATFORM_TAG_REGEX,$PLATFORM_TAG_REGEX,g /etc/prometheus/prometheus.yml
 sed -i s,ENVIRONMENT_TAG_REGEX,$ENVIRONMENT_TAG_REGEX,g /etc/prometheus/prometheus.yml
 sed -i s,CATEGORY_TAG_REGEX,$CATEGORY_TAG_REGEX,g /etc/prometheus/prometheus.yml
+sed -i s,AWS_REGION,$AWS_REGION,g /etc/prometheus/prometheus.yml
+
+cat /etc/prometheus/prometheus.yml
 
 /bin/prometheus "$@"
