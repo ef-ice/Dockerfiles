baec67528960ead9bcc564423c1812c62e9a29d2
vnadgir-ef <vaishakh.nadgir@ef.com>
2017-03-21 14:26:45 +0000

    Update Host header to help with routing

--- efset-nginx-data/docker-entrypoint.sh
+++ efset-nginx-data/docker-entrypoint.sh
@@ -6,8 +6,14 @@ if [ -z "$PROXY_PASS_URL" ]; then
    exit 1
 fi
 
+
+if [ -z "$HOST_HEADER" ]; then
+   echo "Missing HOST_HEADER environment variable"
+   exit 1
+fi
 export NAMESERVERS=$(cat /etc/resolv.conf | grep "nameserver" | awk '{print $2}' | tr '\n' ' ')
 sed -i "s|resolver NAMESERVERS;|resolver $NAMESERVERS;|g" /etc/nginx/nginx.conf
 sed -i "s|PROXY_PASS_URL|$PROXY_PASS_URL|g" /etc/nginx/nginx.conf
+sed -i "s|HOST_HEADER|$HOST_HEADER|g" /etc/nginx/nginx.conf
 
 nginx -g 'daemon off;'
