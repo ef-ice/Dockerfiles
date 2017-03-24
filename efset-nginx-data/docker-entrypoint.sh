2d20903d08c934a5189c8467816f9115000042b5
vnadgir-ef <vaishakh.nadgir@ef.com>
2017-03-24 16:28:20 +0000

    Ability to configure query params as well as Host header for routing purposes

--- efset-nginx-data/docker-entrypoint.sh
+++ efset-nginx-data/docker-entrypoint.sh
@@ -6,14 +6,18 @@ if [ -z "$PROXY_PASS_URL" ]; then
    exit 1
 fi
 
-
 if [ -z "$HOST_HEADER" ]; then
    echo "Missing HOST_HEADER environment variable"
+   exit 1
+fi
+
+if [ -z "$QUERY_PARAM" ]; then
+   echo "Missing QUERY_PARAM environment variable"
    exit 1
 fi
 export NAMESERVERS=$(cat /etc/resolv.conf | grep "nameserver" | awk '{print $2}' | tr '\n' ' ')
 sed -i "s|resolver NAMESERVERS;|resolver $NAMESERVERS;|g" /etc/nginx/nginx.conf
 sed -i "s|PROXY_PASS_URL|$PROXY_PASS_URL|g" /etc/nginx/nginx.conf
-sed -i "s|HOST_HEADER|$HOST_HEADER|g" /etc/nginx/nginx.conf
+sed -i "s|QUERY_PARAM|$QUERY_PARAM|g" /etc/nginx/nginx.conf
 
 nginx -g 'daemon off;'
