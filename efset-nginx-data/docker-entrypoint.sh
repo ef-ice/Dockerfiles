ad0c42a74c315dc40c455f9cc72bcfc85c737f25
vnadgir-ef <vaishakh.nadgir@ef.com>
2017-03-24 17:30:54 +0000

    Substitute Host header

--- efset-nginx-data/docker-entrypoint.sh
+++ efset-nginx-data/docker-entrypoint.sh
@@ -18,6 +18,7 @@ fi
 export NAMESERVERS=$(cat /etc/resolv.conf | grep "nameserver" | awk '{print $2}' | tr '\n' ' ')
 sed -i "s|resolver NAMESERVERS;|resolver $NAMESERVERS;|g" /etc/nginx/nginx.conf
 sed -i "s|PROXY_PASS_URL|$PROXY_PASS_URL|g" /etc/nginx/nginx.conf
+sed -i "s|HOST_HEADER|$HOST_HEADER|g" /etc/nginx/nginx.conf
 sed -i "s|QUERY_PARAM|$QUERY_PARAM|g" /etc/nginx/nginx.conf
 
 nginx -g 'daemon off;'
