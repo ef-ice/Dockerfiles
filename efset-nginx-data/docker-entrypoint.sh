873f6b224f1c9fdbc09efe5e8b4aa06d350823bf
vnadgir-ef <vaishakh.nadgir@ef.com>
2017-06-07 14:05:50 +0100

    Update nginx to support range requests at the media url and rewrite the media_url part

--- efset-nginx-data/docker-entrypoint.sh
+++ efset-nginx-data/docker-entrypoint.sh
@@ -11,10 +11,16 @@ if [ -z "$HOST_HEADER" ]; then
    exit 1
 fi
 
+if [ -z "$MEDIA_URL" ]; then
+   echo "Missing MEDIA_URL environment variable"
+   exit 1
+fi
+
 export NAMESERVERS=$(cat /etc/resolv.conf | grep "nameserver" | awk '{print $2}' | tr '\n' ' ')
 sed -i "s|resolver NAMESERVERS;|resolver $NAMESERVERS;|g" /etc/nginx/nginx.conf
 sed -i "s|PROXY_PASS_URL|$PROXY_PASS_URL|g" /etc/nginx/nginx.conf
 sed -i "s|HOST_HEADER|$HOST_HEADER|g" /etc/nginx/nginx.conf
+sed -i "s|MEDIA_URL|$MEDIA_URL|g" /etc/nginx/nginx.conf
 
 
 nginx -g 'daemon off;'
