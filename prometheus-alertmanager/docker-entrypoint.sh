2166f252f6e4bed0263e64700a2d530b7eda4b5c
mateuszgolab <mathgol@gmail.com>
2017-09-06 13:10:56 +0100

    prometheus alert manager : added email receiver

--- prometheus-alertmanager/docker-entrypoint.sh
+++ prometheus-alertmanager/docker-entrypoint.sh
@@ -8,6 +8,13 @@ if [ -z "$TEAMS_WEB_HOOK" ]; then
    exit 1
 fi
 
+if [ -z "$TEAMS_EMAIL" ]; then
+   echo "Missing TEAMS_EMAIL environment variable"
+   exit 1
+fi
+
+
 sed -i s,TEAMS_WEB_HOOK,$TEAMS_WEB_HOOK,g /etc/alertmanager/alertmanager.yml
+sed -i s,TEAMS_EMAIL,$TEAMS_EMAIL,g /etc/alertmanager/alertmanager.yml
 
 /bin/alertmanager "$@"
