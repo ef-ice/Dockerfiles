357031b02d1a9a2508d73d6c2215a99c84cfc26e
mateuszgolab <mathgol@gmail.com>
2017-09-26 17:48:31 +0100

    alertmanager updates to use email notifications (gmail smtp)

--- prometheus-alertmanager/docker-entrypoint.sh
+++ prometheus-alertmanager/docker-entrypoint.sh
@@ -13,8 +13,20 @@ if [ -z "$TEAMS_EMAIL" ]; then
    exit 1
 fi
 
+if [ -z "EMAIL_ACCOUNT" ]; then
+   echo "Missing EMAIL_ACCOUNT environment variable"
+   exit 1
+fi
+
+if [ -z "EMAIL_AUTH_TOKEN" ]; then
+   echo "Missing EMAIL_AUTH_TOKEN environment variable"
+   exit 1
+fi
+
 
 sed -i s,TEAMS_WEB_HOOK,$TEAMS_WEB_HOOK,g /etc/alertmanager/alertmanager.yml
 sed -i s,TEAMS_EMAIL,$TEAMS_EMAIL,g /etc/alertmanager/alertmanager.yml
+sed -i s,EMAIL_ACCOUNT,EMAIL_ACCOUNT,g /etc/alertmanager/alertmanager.yml
+sed -i s,EMAIL_AUTH_TOKEN,EMAIL_AUTH_TOKEN,g /etc/alertmanager/alertmanager.yml
 
 /bin/alertmanager "$@"
